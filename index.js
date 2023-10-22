9a7ce17abfa9f5a35637d50588082bc9429edf4c
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-22 14:26:45 -0700

    Complete overhaul for better coding-user friendly

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const svgGenerator = require('./lib/svgGenerator');
+const inquirer = require('inquirer');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+const fs = require('fs'); // Require the 'fs' module
 
-svgGenerator.getUserInput();
+const shapes = {
+  Triangle,
+  Circle,
+  Square,
+};
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter the color for the shape:',
+    },
+  ])
+  .then((answers) => {
+    const selectedShape = shapes[answers.shape];
+    if (selectedShape) {
+      const shapeInstance = new selectedShape();
+      shapeInstance.setColor(answers.color);
+      const svg = shapeInstance.render();
+
+      // Define a filename based on the shape and color
+      const filename = `${answers.shape}_${answers.color}.svg`;
+      const filePath = `logos/${filename}`;
+
+      // Write the SVG content to the file
+      fs.writeFile(filePath, svg, (err) => {
+        if (err) {
+          console.error('Error saving the logo:', err);
+        } else {
+          console.log(`Logo saved as ${filename}`);
+        }
+      });
+    } else {
+      console.log('Invalid shape selected.');
+    }
+  });
